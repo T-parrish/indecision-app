bf8a598dc4d916332d2050fb8f76cfbb873f33f0
Taylor Parrish <twpearish@gmail.com>
2018-04-11 16:11:19 +0900

    experiments with stateful components and stateless functional components

--- src/app.js
+++ src/app.js
@@ -4,106 +4,170 @@ console.log('app is running')
 // can run with --watch flag to output changes in realtime
 
 class IndecisionApp extends React.Component {
-    render() {
-        const title = 'Dat app';
-        const subtitle = 'Put your life in the hands of a computer';
-        const options = ['loop', 'zoop'];
-
-        return (
-            <div>
-                <Header title = {title} subtitle={subtitle}/>
-                <Action />
-                <Options options = {options}/>
-                <AddOption />
-            </div>
-        );
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
-}
-
-class Action extends React.Component {
-    handlePick () {
-        alert('The british are coming!')
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handlePick}>what should I do</button>
-            </div>
-        );
-    }
-}
-
-class Options extends React.Component {
     // bare minimum to override constructor function
     // and ensure you have access to this.props
     constructor(props) {
         super(props);
         // Ensures the context stays correct and is 
         // only called once before the component renders
-        this.removeAll = this.removeAll.bind(this);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
+        this.state = {
+            options: props.options
+        };
     }
 
-    removeAll () {
-        alert('trashing your options')
+    componentDidMount() {
+        console.log('component mounted')
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('component updated')
+    }
+    componentWillUnmount() {
+        console.log('component unmounted')
+    }
+
+    // implicitly returns the object containing the options array
+    handleDeleteOptions() {
+        this.setState(() => ({options: []}))
+    }
+
+    handleDeleteOption(optionToRemove) {
+        this.setState((prevState) => ({
+            options:prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+
+    handlePick() {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+
+        alert(option);
+    }
+
+    handleAddOption(option) {
+        if (!option) {
+            return 'Enter valid value to add item'
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists'
+        }
+
+        this.setState((prevState) => ({options: prevState.options.concat([option])}))
     }
 
     render() {
+        const subtitle = 'Put your life in the hands of a computer';
+
         return (
             <div>
-                <p>{this.props.options.length}</p>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option} />)
-                }
-                <button onClick={this.removeAll}>BOOM</button>
-                <Option />
+                <Header subtitle = {subtitle}/>
+                <Action 
+                    hasOptions = {this.state.options.length > 0}
+                    handlePick = {this.handlePick}
+                />
+                <Options 
+                    options = {this.state.options}
+                    handleDeleteOptions = {this.handleDeleteOptions}
+                    handleDeleteOption = {this.handleDeleteOption}
+                />
+                <AddOption 
+                    handleAddOption = {this.handleAddOption}
+                />
             </div>
         );
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+};
 
+// Stateless functional component
+// Faster than other types of components
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
+        </div>
+    );
+}
+
+Header.defaultProps = {
+    title: 'Dat app'
+}
+
+const Action = (props) => {
+    return (
+        <div>
+            <button 
+                onClick={props.handlePick}
+                disabled={!props.hasOptions}
+            >
+                what should I do?
+            </button>
+        </div>
+    );
+}
+
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>BOOM</button>
+            <p>{props.options.length}</p>
+            {
+                props.options.map((option) => 
+                    <Option key={option} 
+                    optionText={option} 
+                    handleDeleteOption={props.handleDeleteOption}
+                />)
+            }
+        </div>
+    );
+}
+
+const Option = (props) => {
+    return (
+        <div>
+            {props.optionText}
+            <button 
+                onClick={(e) => {
+                    props.handleDeleteOption(props.optionText);
+                }}
+            >
+            Delete me
+            </button>
+        </div>
+    );
+}
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            err: undefined
+        }
+    }
     handleAddOption(e) {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim();
+        const err = this.props.handleAddOption(option);
 
-        if (option) {
-            alert(option)
-        }
+        this.setState(() => ({err}))
     }
-    
+
     render() {
         return (
             <div>
+                {this.state.err && <p>{this.state.err}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button>Add Option </button>
                 </form>
-            </div>
-        );
-    }
-}
-
-
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
             </div>
         );
     }
--- src/noodling/stateful-counter.js
+++ src/noodling/stateful-counter.js
@@ -7,7 +7,7 @@ class Counter extends React.Component{
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: 0
+            count: props.count
         };
     }
 
@@ -48,4 +48,8 @@ class Counter extends React.Component{
     }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'))
+Counter.defaultProps = {
+    count: 0
+}
+
+ReactDOM.render(<Counter count={42}/>, document.getElementById('app'))
